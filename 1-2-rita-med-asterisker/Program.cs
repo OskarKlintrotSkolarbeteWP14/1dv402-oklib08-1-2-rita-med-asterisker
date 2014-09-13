af9927cc8e65e059fd6a803961ab728ff72553da
Oskar Klintrot <oskar.klintrot@gmail.com>
2014-09-13 22:44:04 +0200

    Revert "The diamond renders correctly"
    This reverts commit 5251517ee382b9c38e9bcd9491947d8515b471f2.

--- 1-2-rita-med-asterisker/Program.cs
+++ 1-2-rita-med-asterisker/Program.cs
@@ -17,16 +17,11 @@ namespace _1_2_rita_med_asterisker
             do
             {
                 Console.Clear();
-
-                //RenderDiamond renders the diamond (hence the name...) and uses ReadOddByte to  
-                //get an input from user on how large diamond the user wants it to draw.
+                //ReadOddByte(Strings.Asterisk_Prompt, maxValue);
                 RenderDiamond(ReadOddByte(Strings.Asterisk_Prompt, maxValue));
-                Console.WriteLine();
                 Console.Write(Strings.Continue_Prompt);
             } while (IsContinuing());
         }
-
-        //Makes the Main loop until esc is pressed
         private static bool IsContinuing()
         {
             ConsoleKeyInfo esc;
@@ -34,8 +29,6 @@ namespace _1_2_rita_med_asterisker
             return esc.Key != ConsoleKey.Escape;
 
         }
-
-        //Asks the user how large diamond it wants it to draw and also makes sure the input is valid
         private static byte ReadOddByte(string prompt = null, byte maxValue = 255)
         {
             byte readValue = 0;
@@ -45,11 +38,10 @@ namespace _1_2_rita_med_asterisker
                 {
                     Console.Write(prompt, maxValue);
                     byte checkValue = byte.Parse(Console.ReadLine());
-
-                    //Makes sure it's below maxValue and an odd number
                     if (checkValue > maxValue || (checkValue % 2 == 0))
                         throw new OverflowException();
                     readValue = checkValue;
+                    Console.WriteLine(readValue);
                     break;
                 }
                 catch (Exception)
@@ -61,43 +53,25 @@ namespace _1_2_rita_med_asterisker
             }
             return readValue;
         }
-
-        //Renders the diamond, sends data to RenderRow who makes the actual drawing
         private static void RenderDiamond(byte maxCount)
         {
             int addOrSub = 1; 
             int j = 0;
-            for (int i = 0; i + j >= 0; i = i + addOrSub)
+            for (int i = 0; i + j <= maxCount; i = i + addOrSub)
             {
-                if (addOrSub > 0)
-                {
-                    RenderRow(maxCount - i - maxCount / 2, i + j);
-                }
-                else if (i + j != maxCount)
-                {
-                    RenderRow(maxCount - j - maxCount / 2, i + j);
-                }
+                RenderRow(maxCount - i, i + j);
                 j = i;
-                //When the for loop hits maxCount, this if-statement makes it "reverse"
-                if (i + j > maxCount)
-                    addOrSub = -1;
             }
         }
-
-        //Renders a row at the time 
         private static void RenderRow(int maxCount, int asteriskCount)
         {
             for (int i = 0; i < maxCount; i++)
             {
                 Console.Write(" "); 
             }
-
-            int j = 1;
             for (int i = 0; i < asteriskCount; i++)
             {
-                if (j >= 0)
-                    Console.Write("*");
-                j = i;
+                Console.Write("*"); 
             }
             Console.WriteLine(); 
         }
