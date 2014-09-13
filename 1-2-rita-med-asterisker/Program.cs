43fda3909dd4d7711adbe20fcded47b9b4376f76
Oskar Klintrot <oskar.klintrot@gmail.com>
2014-09-13 17:29:38 +0200

    IsContinuing stops the loop in Main if you press escape

---
+++ 1-2-rita-med-asterisker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2_rita_med_asterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Rita med asterisker - nivå C";
+            
+            do
+            {
+                Console.Clear();
+                
+            } while (IsContinuing());
+        }
+        private static bool IsContinuing()
+        {
+            ConsoleKeyInfo esc;
+            esc = Console.ReadKey();
+            return esc.Key != ConsoleKey.Escape;
+
+        }
+        //private static byte ReadOddByte(string prompt = null, byte maxValue = 255)
+        //{
+
+        //}
+        private static void RenderDiamond(byte maxCount)
+        {
+
+        }
+        private static void RenderRow(int maxCount, int asteriskCount)
+        {
+
+        }
+    }
+}
---
+++ 1-2-rita-med-asterisker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("1-2-rita-med-asterisker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("1-2-rita-med-asterisker")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b841d711-67f8-4f2c-9470-df07320bff42")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
